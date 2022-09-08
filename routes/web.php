fea3ea901eef108853edc4b74e92df4e5928bcfa
lucas <lucas.dantas.nogueira@gmail.com>
2022-09-08 11:38:19 -0300

    adicionando rotas para o CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$this->post('comment', 'Posts\CommentController@store')->name('comment.store');
+Route::resource('posts', 'Posts\PostController');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
