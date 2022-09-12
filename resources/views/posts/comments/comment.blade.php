20e76d1520223afa99e0e22d9bfa5ee92a0c704e
lucas <lucas.dantas.nogueira@gmail.com>
2022-09-12 10:26:07 -0300

    adicionando validações de usuario, checando se está conectado e mostrando mensagens de erro, caso tenha dado falha

--- resources/views/posts/comments/comment.blade.php
+++ resources/views/posts/comments/comment.blade.php
@@ -1,5 +1,22 @@
 <hr>
 @if (auth()->check())
+
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>        
+    </div>
+@endif
+
 <form action="{{route('comment.store')}}" class="form" method="POST">
     @csrf
     <input type="hidden" name="post_id" value="{{ $post->id }}">
