e0418749171c486cf150bde4a079854ce713f521
lucas <lucas.dantas.nogueira@gmail.com>
2022-09-12 10:25:15 -0300

    adicionando o comentario no banco e mostrano mensagem de sucesso, caso tenha enviado corretamente

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Posts;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCommentFormRequest;
 
 class CommentController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreCommentFormRequest $request)
     {
-        dd($request->except('_token'));
+        // dd(auth()->user());
+        $comment = $request->user()->comments()->create($request->all());
+
+        return redirect()
+                ->route('posts.show', $comment->post_id)
+                ->withSuccess('Comentário realizado com sucesso!');
     }
 }
