50f7d1e3cb86f893cdef05fcc6855a0de399afc2
lucas <lucas.dantas.nogueira@gmail.com>
2022-09-13 11:49:23 -0300

    config da aplicação e mudanças

---
+++ app/Notifications/PostComment.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PostComment extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $comment;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject("Novo comentário: {$this->comment->title}")
+                    ->line("{$this->comment->body}")
+                    ->action('Ver Comentário', route('posts.show', $this->comment->post_id))
+                    ->line('Obrigado por usar nossa aplicação!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            'comment' => $this->comment,
+        ];
+    }
+}
---
+++ resources/js/components/notifications/Notifications.vue
@@ -0,0 +1,49 @@
+<template>
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Notificações <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#" v-for="notification in notifications" :key="notification.id">
+                {{ notification.data.comment.title }}
+                
+            </a>
+
+            <a class="dropdown-item" href="#">
+                Limpar Notificações
+            </a>
+            
+        </div>
+    </li>
+</template>
+
+<script>
+import Axios from 'axios';
+
+    export default {
+        created () {
+            this.loadNotifications();
+        },
+
+        computed: {
+            notifications() {
+                return this.notificationsItems
+            }
+        },
+
+        data() {
+            return {
+                notificationsItems: []
+            }
+        },
+
+        methods: {
+            loadNotifications () {
+                axios.get('/notifications')
+                        .then(response => this.notificationsItems = response.data)
+            }
+        },
+    }
+</script>
