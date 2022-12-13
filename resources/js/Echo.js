31207b3220cf7c5c6b1dee612fc8f5c1769bea85
lucas <lucas.dantas.nogueira@gmail.com>
2022-12-13 11:44:16 -0300

    Adicionando validação de User e um alerta de notificação quando for enviada

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,8 +1,30 @@
+import store from './vuex/store';
+import swal from 'sweetalert';
+
+const typesNotifications = {
+    postComment: 'App\\Notifications\\PostComment',
+}
+
+console.log(Laravel.user);
+
 if (Laravel.user) {
-    console.log(`App.Models.User.${Laravel.user}`);
     window.Echo.private(`App.Models.User.${Laravel.user}`)
             .notification(notification => {
-                console.log(notification);
+                let comment = notification.data.comment;
+
+                if (Laravel.user == comment.post.user.id) {
+                    if (notification.type == typesNotifications.postComment) {
+                        swal({
+                            title: "Novo comentário",
+                            text: `O post ${comment.post.title} foi comentado por ${comment.user.name}`,
+                            icon: 'info'
+                        })
+
+                        store.commit('ADD_NOTIFICATION', notification);
+                    }
+                }
+
+                
             })
 }
 
