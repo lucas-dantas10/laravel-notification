14ee9d3c3da66ec73a283577b6183989e340e7e3
lucas <lucas.dantas.nogueira@gmail.com>
2022-12-13 12:03:52 -0300

    Adicionando uma Mutation para renderizar a notificacao enviada sem recarregar a página

--- resources/js/vuex/modules/notifications.js
+++ resources/js/vuex/modules/notifications.js
@@ -6,11 +6,12 @@ export default {
     },
 
     mutations: {
-        LOAD_NOTIFICATION(state, notifications) {
+
+        LOAD_NOTIFICATION (state, notifications) {
             state.items = notifications
         },
 
-        MARK_AS_READ(state, id) {
+        MARK_AS_READ (state, id) {
             let index = state.items.filter(notification => {
                 notification.id == id;
             });
@@ -18,25 +19,30 @@ export default {
             state.items.splice(index, 1);
         },
 
-        MARK_ALL_AS_READ(state) {
+        MARK_ALL_AS_READ (state) {
             state.items = [];
+        },
+
+        ADD_NOTIFICATION (state, notification) {
+            state.items.push(notification);
         }
+        
     },
 
     actions: {
-        loadNotifications(context) {
+        loadNotifications (context) {
             Axios.get('/notifications')
                 .then(response => {
                     context.commit('LOAD_NOTIFICATION', response.data)
                 })
         },
 
-        markAsRead(context, params) {
+        markAsRead (context, params) {
             console.log(context);
             Axios.put('/notification-read', params).then(() => context.commit('MARK_AS_READ', params))
         },
 
-        markAllAsRead(context) {
+        markAllAsRead (context) {
             console.log(context);
             Axios.put('/notification-all-read')
                     .then(() => context.commit('MARK_ALL_AS_READ'));
