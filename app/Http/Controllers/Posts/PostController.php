5a8e8efeb03fb947549617a3a2f3cf744c8d02c8
lucas <lucas.dantas.nogueira@gmail.com>
2022-12-14 08:41:14 -0300

    Criando métodos de destroy e update

--- app/Http/Controllers/Posts/PostController.php
+++ app/Http/Controllers/Posts/PostController.php
@@ -17,11 +17,23 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = $this->post->with('comments')->paginate(15);
+        $posts = $this->post->with('comments')->paginate(7);
 
         return view('posts.index', [
             'posts' => $posts
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->user()->posts()->create($request->all());
+
+        return redirect()->route('posts.index');
     }
 
     public function show($id)
@@ -31,5 +43,44 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function edit($id)
+    {
+        $post = $this->post->find($id);
+
+        return view('posts.edit', [
+            'post' => $post
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {   
+        $post = $this->post->find($id);
+
+        $post->update([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect('posts');
+    }
+
+    public function delete($id)
+    {
+        $post = $this->post->with(['comments.user', 'user'])->find($id);
+
+        return view('posts.delete', [
+            'post' => $post
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $post = $this->post->find($id);
+
+        $post->delete();
+
+        return redirect('posts');
     }
 }
